01a1eb06631584941c7916a181b30158d25d9642
Fillipy <57646927+Fillipyp@users.noreply.github.com>
2021-08-05 17:44:06 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown();
+
+  }); 
+  
+})(jQuery); // end of jQuery name space
